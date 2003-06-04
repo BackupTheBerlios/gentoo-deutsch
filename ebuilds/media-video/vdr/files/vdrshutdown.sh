cf465da42e647f8702c89aa1137de2f3e50e1f97
mad <mad>
2003-06-04 11:47:18 +0000

    typo fix

--- ebuilds/media-video/vdr/files/vdrshutdown.sh
+++ ebuilds/media-video/vdr/files/vdrshutdown.sh
@@ -2,13 +2,11 @@
 #
 # sample vdrshutdown script (needs nvram-wakeup)
 #
-
 if [ "$1" -ne "0" ]; then
 	/usr/bin/nvram-wakeup -s$1 -l -C /etc/nvram-wakeup.conf
-	lilo -R PowerOff
-	reboot
-
+	/sbin/lilo -R PowerOff
+	/sbin/reboot
 else
 	/usr/bin/nvram-wakeup -d
-	poweroff
+	/sbin/poweroff
 fi
