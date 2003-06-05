25cbeb92c8eb3c361f570b4bc814bd51f0ee6f58
mad <mad>
2003-06-05 10:23:14 +0000

    added hint for sudo

--- ebuilds/media-video/vdr/files/vdrshutdown.sh
+++ ebuilds/media-video/vdr/files/vdrshutdown.sh
@@ -2,6 +2,12 @@
 #
 # sample vdrshutdown script (needs nvram-wakeup)
 #
+# If vdr runs with UID !root you should add:
+# vdr ALL=(ALL) NOPASSWD: /usr/bin/vdrshutdown.sh
+# to your sudousers (visudo as root)
+#
+#
+
 if [ "$1" -ne "0" ]; then
 	/usr/bin/nvram-wakeup -s$1 -l -C /etc/nvram-wakeup.conf
 	/sbin/lilo -R PowerOff
